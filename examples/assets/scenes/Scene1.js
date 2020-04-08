effaca864c13dd52b0ad5ab1bf15622226d065ac
UltimatePro-Grammer <63370104+UltimatePro-Grammer@users.noreply.github.com>
2020-04-08 15:28:57 -0400

    Add Scene js File

---
+++ examples/assets/scenes/Scene1.js
@@ -0,0 +1,378 @@
+
+// You can write more code here
+
+/* START OF COMPILED CODE */
+
+class Scene1 extends Phaser.Scene {
+	
+	constructor() {
+	
+		super("Scene1");
+		
+	}
+	
+	_create() {
+	
+		this.add.image(400.53632, 224.79376, "bg");
+		
+		this.add.image(400.955, 659.2897, "bg");
+		
+		this.add.image(1188.018, 226.18517, "bg");
+		
+		this.add.image(1192.415, 668.0836, "bg");
+		
+		this.add.image(-390.11633, 223.64653, "bg");
+		
+		this.add.image(-393.46722, 665.9602, "bg");
+		
+		this.add.image(-391.33353, -198.29831, "bg");
+		
+		this.add.image(1176.6853, -193.56111, "bg");
+		
+		this.add.image(404.51898, -193.56111, "bg");
+		
+		this.add.image(-386.5963, -577.27576, "bg");
+		
+		this.add.image(404.51895, -567.80133, "bg");
+		
+		this.add.image(1171.9482, -558.32684, "bg");
+		
+		this.add.image(397.34174, 1847.5309, "bg");
+		
+		this.add.image(397.7604, 2282.0269, "bg");
+		
+		this.add.image(1184.8234, 1848.9224, "bg");
+		
+		this.add.image(1189.2205, 2290.8208, "bg");
+		
+		this.add.image(-393.3109, 1846.3837, "bg");
+		
+		this.add.image(-396.6618, 2288.6973, "bg");
+		
+		this.add.image(-394.52808, 1424.4388, "bg");
+		
+		this.add.image(1173.4907, 1429.176, "bg");
+		
+		this.add.image(401.3244, 1429.176, "bg");
+		
+		this.add.image(-389.7909, 1045.4614, "bg");
+		
+		this.add.image(401.32437, 1054.9359, "bg");
+		
+		this.add.image(1168.7537, 1064.4103, "bg");
+		
+		this.add.image(-1925.1052, 237.18808, "bg");
+		
+		this.add.image(-1924.6865, 671.68396, "bg");
+		
+		this.add.image(-1137.6237, 238.5795, "bg");
+		
+		this.add.image(-1133.2266, 680.4779, "bg");
+		
+		this.add.image(-2715.7578, 236.04086, "bg");
+		
+		this.add.image(-2719.109, 678.3545, "bg");
+		
+		this.add.image(-2716.975, -185.90402, "bg");
+		
+		this.add.image(-1148.9563, -181.16678, "bg");
+		
+		this.add.image(-1921.1226, -181.16678, "bg");
+		
+		this.add.image(-2712.2378, -564.8815, "bg");
+		
+		this.add.image(-1921.1226, -555.407, "bg");
+		
+		this.add.image(-1153.6934, -545.9326, "bg");
+		
+		this.add.image(-1928.2998, 1859.9252, "bg");
+		
+		this.add.image(-1927.8811, 2294.4211, "bg");
+		
+		this.add.image(-1140.8182, 1861.3167, "bg");
+		
+		this.add.image(-1136.4211, 2303.215, "bg");
+		
+		this.add.image(-2718.9524, 1858.778, "bg");
+		
+		this.add.image(-2722.3035, 2301.0916, "bg");
+		
+		this.add.image(-2720.1697, 1436.8331, "bg");
+		
+		this.add.image(-1152.1509, 1441.5703, "bg");
+		
+		this.add.image(-1924.3171, 1441.5703, "bg");
+		
+		this.add.image(-2715.4324, 1057.8557, "bg");
+		
+		this.add.image(-1924.3171, 1067.3302, "bg");
+		
+		this.add.image(-1156.888, 1076.8046, "bg");
+		
+		this.add.image(5065.134, 237.18802, "bg");
+		
+		this.add.image(5065.5527, 671.6839, "bg");
+		
+		this.add.image(5852.6157, 238.57944, "bg");
+		
+		this.add.image(5857.0127, 680.47784, "bg");
+		
+		this.add.image(4274.4814, 236.0408, "bg");
+		
+		this.add.image(4271.1304, 678.35443, "bg");
+		
+		this.add.image(4273.264, -185.90405, "bg");
+		
+		this.add.image(5841.2827, -181.16685, "bg");
+		
+		this.add.image(5069.1167, -181.16685, "bg");
+		
+		this.add.image(4278.0015, -564.88153, "bg");
+		
+		this.add.image(5069.1167, -555.4071, "bg");
+		
+		this.add.image(5836.546, -545.9326, "bg");
+		
+		this.add.image(5061.9395, 1859.9252, "bg");
+		
+		this.add.image(5062.358, 2294.4211, "bg");
+		
+		this.add.image(5849.421, 1861.3165, "bg");
+		
+		this.add.image(5853.818, 2303.215, "bg");
+		
+		this.add.image(4271.2866, 1858.778, "bg");
+		
+		this.add.image(4267.9355, 2301.0916, "bg");
+		
+		this.add.image(4270.0693, 1436.8331, "bg");
+		
+		this.add.image(5838.0884, 1441.5702, "bg");
+		
+		this.add.image(5065.922, 1441.5702, "bg");
+		
+		this.add.image(4274.8066, 1057.8556, "bg");
+		
+		this.add.image(5065.922, 1067.3302, "bg");
+		
+		this.add.image(5833.351, 1076.8046, "bg");
+		
+		this.add.image(2739.4924, 249.5823, "bg");
+		
+		this.add.image(2739.9111, 684.0782, "bg");
+		
+		this.add.image(3526.974, 250.97379, "bg");
+		
+		this.add.image(3531.371, 692.87213, "bg");
+		
+		this.add.image(1948.8398, 248.43509, "bg");
+		
+		this.add.image(1945.4888, 690.7487, "bg");
+		
+		this.add.image(1947.6226, -173.50977, "bg");
+		
+		this.add.image(3515.6414, -168.77252, "bg");
+		
+		this.add.image(2743.475, -168.77252, "bg");
+		
+		this.add.image(1952.3599, -552.48724, "bg");
+		
+		this.add.image(2743.475, -543.01276, "bg");
+		
+		this.add.image(3510.9043, -533.5384, "bg");
+		
+		this.add.image(2736.2979, 1872.3195, "bg");
+		
+		this.add.image(2736.7166, 2306.8154, "bg");
+		
+		this.add.image(3523.7793, 1873.7108, "bg");
+		
+		this.add.image(3528.1765, 2315.6094, "bg");
+		
+		this.add.image(1945.6453, 1871.1722, "bg");
+		
+		this.add.image(1942.2942, 2313.4858, "bg");
+		
+		this.add.image(1944.428, 1449.2274, "bg");
+		
+		this.add.image(3512.4468, 1453.9645, "bg");
+		
+		this.add.image(2740.2805, 1453.9645, "bg");
+		
+		this.add.image(1949.1653, 1070.2499, "bg");
+		
+		this.add.image(2740.2805, 1079.7245, "bg");
+		
+		this.add.image(3507.7097, 1089.1989, "bg");
+		
+		this.add.image(1547.8162, -2948.0227, "bg");
+		
+		this.add.image(1548.2351, -2513.5269, "bg");
+		
+		this.add.image(2335.298, -2946.6313, "bg");
+		
+		this.add.image(2339.695, -2504.733, "bg");
+		
+		this.add.image(757.1638, -2949.17, "bg");
+		
+		this.add.image(753.81274, -2506.8562, "bg");
+		
+		this.add.image(755.94653, -3371.1147, "bg");
+		
+		this.add.image(2323.9656, -3366.3777, "bg");
+		
+		this.add.image(1551.7991, -3366.3777, "bg");
+		
+		this.add.image(760.68384, -3750.0923, "bg");
+		
+		this.add.image(1551.7991, -3740.618, "bg");
+		
+		this.add.image(2319.2283, -3731.1433, "bg");
+		
+		this.add.image(1544.6218, -1325.2855, "bg");
+		
+		this.add.image(1545.0403, -890.7897, "bg");
+		
+		this.add.image(2332.1033, -1323.8942, "bg");
+		
+		this.add.image(2336.5007, -881.9957, "bg");
+		
+		this.add.image(753.969, -1326.4327, "bg");
+		
+		this.add.image(750.6184, -884.11926, "bg");
+		
+		this.add.image(752.7517, -1748.3776, "bg");
+		
+		this.add.image(2320.7708, -1743.6403, "bg");
+		
+		this.add.image(1548.6042, -1743.6403, "bg");
+		
+		this.add.image(757.489, -2127.3552, "bg");
+		
+		this.add.image(1548.6042, -2117.8806, "bg");
+		
+		this.add.image(2316.034, -2108.4062, "bg");
+		
+		this.add.image(-777.8252, -2935.6284, "bg");
+		
+		this.add.image(-777.4065, -2501.1326, "bg");
+		
+		this.add.image(9.656492, -2934.237, "bg");
+		
+		this.add.image(14.053465, -2492.3386, "bg");
+		
+		this.add.image(-1568.4778, -2936.7756, "bg");
+		
+		this.add.image(-1571.8289, -2494.4622, "bg");
+		
+		this.add.image(-1569.6951, -3358.7205, "bg");
+		
+		this.add.image(-1.6762714, -3353.9832, "bg");
+		
+		this.add.image(-773.8425, -3353.9832, "bg");
+		
+		this.add.image(-1564.9578, -3737.698, "bg");
+		
+		this.add.image(-773.8425, -3728.2234, "bg");
+		
+		this.add.image(-6.413332, -3718.749, "bg");
+		
+		this.add.image(-781.0198, -1312.8912, "bg");
+		
+		this.add.image(-780.6011, -878.39514, "bg");
+		
+		this.add.image(6.461668, -1311.4996, "bg");
+		
+		this.add.image(10.858885, -869.6012, "bg");
+		
+		this.add.image(-1571.6724, -1314.0385, "bg");
+		
+		this.add.image(-1575.0234, -871.72473, "bg");
+		
+		this.add.image(-1572.8896, -1735.9833, "bg");
+		
+		this.add.image(-4.8708515, -1731.2462, "bg");
+		
+		this.add.image(-777.0371, -1731.2462, "bg");
+		
+		this.add.image(-1568.1523, -2114.9607, "bg");
+		
+		this.add.image(-777.0371, -2105.4863, "bg");
+		
+		this.add.image(-9.607912, -2096.012, "bg");
+		
+		
+	}
+	
+	/* START-USER-CODE */
+
+	create() {
+		// vignette
+		this.Vignette = this.game.renderer.addPipeline('Vignette', new Vignette(this.game));
+		this.applyPipeline(); 
+		
+		player = this.physics.add.sprite(300,300, 'bloomPlayer').setDepth(2).setScale(2).setSize(55, 55).setOffset(20,20).setDrag(800,800);
+		var enemy = this.physics.add.sprite(700,300, 'bloomEnemy').setDepth(1).setScale(2).setSize(55, 55).setOffset(20,20).setDrag(800,800);
+		setInterval(()=>{
+			this.physics.moveToObject(enemy, player, 900);
+		}, 1000)
+		this.cameras.main.startFollow(player);
+		
+		this.physics.add.overlap(enemy, player, ()=>{
+			
+		}, null, this)
+		
+		arrowKeys = this.input.keyboard.createCursorKeys();
+        cursors = this.input.keyboard.addKeys(
+        {up:Phaser.Input.Keyboard.KeyCodes.W,
+        down:Phaser.Input.Keyboard.KeyCodes.S,
+        left:Phaser.Input.Keyboard.KeyCodes.A,
+        right:Phaser.Input.Keyboard.KeyCodes.D,
+        e:Phaser.Input.Keyboard.KeyCodes.E
+        });
+		this._create();
+	}
+	
+	applyPipeline() {
+		 this.Vignette.setFloat2('resolution', 800, 450);
+		 this.Vignette.setFloat1('r',0.2);
+		 this.Vignette.setFloat1('b',0.9);
+		 this.Vignette.setFloat1('tx', 0.8);
+		 this.Vignette.setFloat1('ty', 0.8);
+		 this.Vignette.setFloat1('bright', 1.0);
+		 this.Vignette.setFloat1('red', 1.0);
+		 this.Vignette.setFloat1('green', 1.0);
+		 this.Vignette.setFloat1('blue', 1.0);
+	
+		 this.Vignette.setFloat1('bgred', 1.0);
+		 this.Vignette.setFloat1('bggreen', 1.0);
+		 this.Vignette.setFloat1('bgblue', 1.0);
+         this.cameras.main.setRenderToTexture(this.Vignette);
+    }
+
+	update() {
+        if (cursors.left.isDown /*|| joysticks.left.isDown */|| arrowKeys.left.isDown)
+        {
+                player.setVelocityX(-500);
+        }
+        if (cursors.right.isDown /*|| joysticks.right.isDown */|| arrowKeys.right.isDown)
+        {
+                player.setVelocityX(500);
+        }
+		if (cursors.up.isDown /*|| joysticks.left.isDown */|| arrowKeys.up.isDown)
+        {
+                player.setVelocityY(-500);
+        }
+        if (cursors.down.isDown /*|| joysticks.right.isDown */|| arrowKeys.down.isDown)
+        {
+                player.setVelocityY(500);
+        }
+	}
+
+
+	/* END-USER-CODE */
+}
+
+/* END OF COMPILED CODE */
+
+// You can write more code here
